b1e628846f87a5ed82dcda463a4993957e56a498
Enricco Gemha <riccogemha@gmail.com>
2024-01-01 17:19:01 -0600

    footer issue solved

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+// ################## Initiate Vanilla Tilt ##################
+
 VanillaTilt.init(
   document.querySelectorAll(".js-tilt"), {
     max: 25,
     speed: 400,
   }
 );
+
+// ################## Color the active nav link ##################
+
+const navLinks = document.getElementsByClassName('nav-link');
+
+var path = window.location.pathname;
+path = path.split("/").pop();
+path = path.split(".")[0];
+const activeLink = document.getElementById(path);
+
+console.log(path)
+if (activeLink) {
+    activeLink.classList.add('active');
+    // Remove the nav-link-active from the other links
+    for (let i = 0; i < navLinks.length; i++) {
+        if (navLinks[i] !== activeLink) {
+            navLinks[i].classList.remove('active');
+        }
+    }
+}
