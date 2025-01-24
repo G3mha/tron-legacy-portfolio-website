cf986298d4f325ff0912e9583d00ae0f2855c2fc
Enricco Gemha <enriccogemha@gmail.com>
2025-01-23 23:32:30 -0500

    v2: Add vanilla-tilt package

---
+++ v2/portfolio/src/hooks/useTilt.js
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react';
+import VanillaTilt from '../vanilla-tilt';
+
+export const useTilt = (options = {}) => {
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const element = elementRef.current;
+    VanillaTilt.init(element, {
+      max: 25,
+      speed: 400,
+      ...options,
+    });
+
+    return () => element.vanillaTilt.destroy();
+  }, [options]);
+
+  return elementRef;
+};
