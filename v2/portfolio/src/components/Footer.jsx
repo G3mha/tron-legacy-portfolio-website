717b51450f1c9394c55d56ab676f9eafe4bc55aa
Enricco Gemha <enriccogemha@gmail.com>
2025-01-23 23:35:54 -0500

    v2: Create components `Header.jsx` and `Footer.jsx`

---
+++ v2/portfolio/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => (
+  <footer>
+    <a href="https://github.com/G3mha">
+      <img src="/img/icon-github.svg" className="icon" alt="GitHub" />
+    </a>
+    <a href="https://www.linkedin.com/in/enriccogemha">
+      <img src="/img/icon-linkedin.svg" className="icon" alt="LinkedIn" />
+    </a>
+    <a href="https://twitter.com/gemhadventures">
+      <img src="/img/icon-x.svg" className="icon" alt="X" />
+    </a>
+    <a href="https://www.instagram.com/enriccogemha">
+      <img src="/img/icon-instagram.svg" className="icon" alt="Instagram" />
+    </a>
+  </footer>
+);
+  
+export default Footer;
